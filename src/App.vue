a02644b0032f981213e7cc9c3d498d69b7ba2d31
Heath Morrison <heath@doublemarked.com>
2018-05-14 10:06:32 +0300

    Added lang

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ export default {
   name: 'app',
 }
 </script>
-<style>
+<style lang="postcss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
